0c98f1330e09017b17dee24d52cd8d55d5c8e647
Adam Wenham <adamwenham64@gmail.com>
2013-08-20 07:36:21 +0100

    reached 23:54 in video, tests still failing but getting closer

--- lib/streamers.ex
+++ lib/streamers.ex
@@ -1,2 +1,13 @@
 defmodule Streamers do
+  def find_index(directory) do
+    files = Path.join(directory, "*.m3u8")
+    Enum.find Path.wildcard(files), is_index?(&1)
+  end
+
+  defp is_index?(file) do
+    File.open! file, fn
+      "#EXTM3U\n#EXT-X-STREAM-INF" <> _ -> true
+      _ -> false
+    end
+  end
 end
--- test/streamers_test.exs
+++ test/streamers_test.exs
@@ -1,7 +1,8 @@
 defmodule StreamersTest do
   use ExUnit.Case
 
-  test "the truth" do
-    assert(true)
+  test "find index file in a directory" do
+    assert Streamers.find_index("tests/fixtures/emberjs") ==
+      "9af0270acb795f9dcafb5c51b1907628.m38u"
   end
 end
